2ebfaa8074dd01acefa174ffa12a5d936ef3640c
Deepak Bhausaheb Patil <dp@Deepaks-MacBook-Air.local>
2025-03-19 21:31:37 +0530

    resolved mongo error

--- FARMS-main/backend/Config/database.js
+++ FARMS-main/backend/Config/database.js
@@ -1,11 +1,16 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
- 
-const connectDatabase=()=>{
+
+const connectDatabase = () => {
     mongoose.connect(process.env.DATABASE_URL)
-    .then(()=>{console.log("Database connected succesfully")})
-    .catch((e)=>{console.log("Issue in server connection");
-        console.error(e.message);
-        process.exit(1)});
-}
-module.exports=connectDatabase;
+        .then(() => {
+            console.log("Database connected successfully");
+        })
+        .catch((e) => {
+            console.log("Issue in server connection");
+            console.error(e.message);
+            process.exit(1); // Exit the process if the connection fails
+        });
+};
+
+module.exports = connectDatabase;
